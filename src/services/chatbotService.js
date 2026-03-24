0ed84564b2cdbf2756492d2631b0b2c5bf4120ce
omarapp19 <omarapp1921@gmail.com>
2026-03-24 15:20:46 -0400

    AHORA SI

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -71,7 +71,7 @@ export async function enviarMensajeChatbot(mensajeUsuario) {
 
     // 3. Llamada a la API REST de Gemini
     try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
